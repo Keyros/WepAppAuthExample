feea167ef54622c5720d31f109b6f1bf73478c8f
Rasim Keita <keita.rasim@gmail.com>
2022-05-10 03:39:23 +0300

    * Create migration project

---
+++ WebApp.Dal.Migrations/WebAppContextFactory.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace WebApp.Dal.Migrations;
+
+// ReSharper disable once UnusedType.Global
+public class WebAppContextFactory : IDesignTimeDbContextFactory<WebAppDbContext>
+{
+    public WebAppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<WebAppDbContext>();
+        optionsBuilder.UseSqlite("Data Source=test.db");
+
+        return new WebAppDbContext(optionsBuilder.Options);
+    }
+}
