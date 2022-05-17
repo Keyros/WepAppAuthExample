773b7555f201d7ebd8419d316c0a0085df251ed9
Rasim Keita <keita.rasim@gmail.com>
2022-05-17 20:39:50 +0300

    * UpdateDb

--- WebApp.Dal/Migrations/20220517173852_LastChanges.Designer.cs
+++ WebApp.Dal/Migrations/20220517173852_LastChanges.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApp.Dal;
 
@@ -10,9 +11,10 @@ using WebApp.Dal;
 namespace WebApp.Dal.Migrations
 {
     [DbContext(typeof(WebAppDbContext))]
-    partial class WebAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220517173852_LastChanges")]
+    partial class LastChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.4");
---
+++ WebApp.Dal/Migrations/20220517173852_LastChanges.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Dal.Migrations
+{
+    public partial class LastChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "RefreshTokenLifeTime",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenLifeTime",
+                table: "Accounts");
+        }
+    }
+}
