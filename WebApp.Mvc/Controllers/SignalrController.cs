030059a1390d33d80718671f4c755d18ac4f2113
Rasim Keita <rke@leapwork.com>
2022-05-24 16:11:41 +0300

    * added view for chat

---
+++ WebApp.Mvc/Controllers/SignalrController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Mvc.Controllers;
+
+[Authorize]
+public class SignalrController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
