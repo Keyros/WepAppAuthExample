b87ba2706720c3cf42bfe0e534fbd62840198fad
Rasim Keita <keita.rasim@gmail.com>
2022-05-24 17:13:17 +0300

    * notification if when connected

--- WebApp.Mvc/SignalR/SimpleChatHub.cs
+++ WebApp.Mvc/SignalR/SimpleChatHub.cs
@@ -8,4 +8,11 @@ public class SimpleChatHub : Hub
     {
         await Clients.All.SendAsync("ReceiveMessage", user, message);
     }
+
+    public override async Task OnConnectedAsync()
+    {
+        var userName = Context.User.Identity.Name;
+        await Clients.Others.SendAsync("ReceiveMessage", userName, "I - connected");
+        await base.OnConnectedAsync();
+    }
 }
