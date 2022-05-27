116838e8cb6a6f2dec8b5d7323689f7c74532019
Rasim Keita <keita.rasim@gmail.com>
2022-05-27 13:32:03 +0300

    * update generator

--- WebApp.RandomStringGenerator/RandomStringGenerator.cs
+++ WebApp.RandomStringGenerator/RandomStringGenerator.cs
@@ -9,6 +9,5 @@ public class RandomStringGenerator
         const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         return new string(Enumerable.Repeat(chars, _random.Next(1, 10))
             .Select(s => s[_random.Next(s.Length)]).ToArray());
-        return string.Empty;
     }
 }
