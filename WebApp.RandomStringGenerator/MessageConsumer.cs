9c0b7c3418193c0fec8bd881946107c48d9b3cf0
Rasim Keita <keita.rasim@gmail.com>
2022-05-27 13:32:54 +0300

    * added some logic

---
+++ WebApp.RandomStringGenerator/MessageConsumer.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace WebApp.RandomStringGenerator;
+
+public class MessageConsumer : IDisposable
+{
+    private readonly string _queueName;
+    private readonly IConnection _connection;
+    private readonly IModel _channel;
+
+    public MessageConsumer(string hostName, string queueName, int port)
+    {
+        _queueName = queueName;
+        var factory = new ConnectionFactory {HostName = hostName, Port = port, UserName = "admin", Password = "admin"};
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+        _channel.QueueDeclare(queue: queueName,
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+    }
+
+    public void Start()
+    {
+        var consumer = new EventingBasicConsumer(_channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(" [x] Received {0}", message);
+        };
+        _channel.BasicConsume(queue: _queueName,
+            autoAck: true,
+            consumer: consumer);
+    }
+
+    public void Dispose()
+    {
+        _channel.Dispose();
+        _connection.Dispose();
+    }
+}
--- WebApp.RandomStringGenerator/Program.cs
+++ WebApp.RandomStringGenerator/Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using WebApp.RandomStringGenerator;
+
 Console.WriteLine("Hello, World!");
+var consumer = new MessageConsumer("localhost", "test", 5672);
+consumer.Start();
+
+await RunProducer();
+
+
+async Task RunProducer()
+{
+    var rndGenerator = new RandomStringGenerator();
+    using (var messageProducer = new MessageProducer("localhost", "test", 5672))
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            messageProducer.SendMessage(
+                Message.Create($"Привет {i}{Environment.NewLine}{rndGenerator.GenerateNextString()}"));
+            await Task.Delay(1000);
+        }
+    }
+}
