d1e0554f396e02bf50afe137893137cc0e288425
Rasim Keita <keita.rasim@gmail.com>
2022-05-27 11:27:27 +0300

    * add new proj

---
+++ WebApp.RandomStringGenerator/RandomStringGenerator.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp.RandomStringGenerator;
+
+public class RandomStringGenerator
+{
+    private readonly Random _random = new();
+
+    public string GenerateNextString()
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, _random.Next(1, 10))
+            .Select(s => s[_random.Next(s.Length)]).ToArray());
+        return string.Empty;
+    }
+}
